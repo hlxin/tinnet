f113a134aa041276bd281a2051c17117aff77791
Hongliang Xin <hongliang06@gmail.com>
2020-11-16 22:02:40 -0500

    clean

--- setup.py
+++ setup.py
@@ -7,20 +7,20 @@ try:
     from numpy.distutils.core import Extension, setup
 except ImportError:
     msg = ("Please install numpy (version 1.7.0 or greater) before installing "
-           "Piml. (Piml uses numpy's installer so it can compile the fortran "
+           "TinNet. (TinNet uses numpy's installer so it can compile the fortran "
            "modules with f2py.) You should be able to do this with a command"
            " like:"
            "   $ pip install numpy")
     raise RuntimeError(msg)
 
 
-name = 'timl'
-version = open(os.path.join('timl', 'VERSION')).read().strip()
+name = 'tinnet'
+version = open(os.path.join('tinnet', 'VERSION')).read().strip()
 description = 'Physics-Informed Machine Learning'
 long_description = open('README').read()
-packages = ['timl', 'timl.feature', 'timl.regression',
-            'timl.model', 'timl.stats']
-Package_dir = {'timl': 'timl', 'feature': 'feature',
+packages = ['tinnet', 'tinnet.feature', 'tinnet.regression',
+            'tinnet.model', 'tinnet.stats']
+package_dir = {'tinnet': 'tinnet', 'feature': 'feature',
                'regression': 'regression', 'model': 'model', 'stats': 'stats'}
 classifiers = ['Programming Language :: Python',
                'Programming Language :: Python :: 2.6',
@@ -29,18 +29,18 @@ classifiers = ['Programming Language :: Python',
                'Programming Language :: Python :: 3.3']
 install_requires = ['numpy>=1.7.0', 'matplotlib', 'ase', 'pyzmq',
                     'pexpect']
-ext_modules = [Extension(name='timl.fmodules',
-                         sources=['timl/model/neuralnetwork.f90',
-                                  'timl/feature/gaussian.f90',
-                                  'timl/feature/cutoffs.f90',
-                                  'timl/feature/zernike.f90',
-                                  'timl/model.f90'])]
+ext_modules = [Extension(name='tinnet.fmodules',
+                         sources=['tinnet/model/neuralnetwork.f90',
+                                  'tinnet/feature/gaussian.f90',
+                                  'tinnet/feature/cutoffs.f90',
+                                  'tinnet/feature/zernike.f90',
+                                  'tinnet/model.f90'])]
 author = 'Hongliang Xin'
 author_email = 'hxin@vt.edu'
-url = 'https://github.com/hlxin/timl'
-package_data = {'timl': ['VERSION']}
+url = 'https://github.com/hlxin/tinnet'
+package_data = {'tinnet': ['VERSION']}
 
-scripts = ['tools/timl-compress', 'tools/timl-plotconvergence']
+scripts = ['tools/tinnet-compress', 'tools/tinnet-plotconvergence']
 
 try:
     setup(name=name,
@@ -59,7 +59,7 @@ try:
           package_data=package_data,
           )
 except SystemExit as ex:
-    if 'timl.fmodules' in ex.args[0]:
+    if 'tinnet.fmodules' in ex.args[0]:
         warnings.warn('It looks like no fortran compiler is present. Retrying '
                       'installation without fortran modules.')
     else:
@@ -79,5 +79,5 @@ except SystemExit as ex:
           url=url,
           package_data=package_data,
           )
-    warnings.warn('Installed Timl without fortran modules since no fortran '
+    warnings.warn('Installed TinNet without fortran modules since no fortran '
                   'compiler was found. The code may run slow as a result.')
